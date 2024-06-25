c81aad728928998b86e85ca054d6c0a5c1df862d
Marshall <mellisgibbs@gmail.com>
2024-06-24 20:17:57 -0400

    finished create()

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -20,14 +20,14 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     {
         super(dataSource);
     }
-
+    
     @Override
     public List<Category> getAllCategories()
     {
         List<Category> categories = new ArrayList<>();
-
+        
         String sql = "SELECT * FROM categories";
-
+        
         try(
                 Connection connection = getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql);
@@ -37,7 +37,7 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
             while(row.next())
             {
                 Category category = mapRow(row);
-
+                
                 categories.add(category);
             }
         }
@@ -45,22 +45,22 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         {
             throw new RuntimeException(e);
         }
-
+        
         return categories;
     }
-
+    
     @Override
     public Category getById(int categoryId)
     {
         String sql = "SELECT * FROM categories WHERE category_id = ?";
-
+        
         try(
                 Connection connection = getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql)
         )
         {
             statement.setInt(1, categoryId);
-
+            
             try(ResultSet row = statement.executeQuery();)
             {
                 if(row.next())
@@ -75,40 +75,68 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         }
         return null;
     }
-
+    
     @Override
     public Category create(Category category)
     {
-        // create a new category
+        String sql = "INSERT INTO categories(name, description) VALUES (?, ?);";
+        
+        try(
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        )
+        {
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            
+            int rowsAffected = statement.executeUpdate();
+            
+            if(rowsAffected > 0)
+            {
+                try(ResultSet generatedKeys = statement.getGeneratedKeys();)
+                {
+                    if(generatedKeys.next())
+                    {
+                        int categoryId = generatedKeys.getInt(1);
+                        
+                        return getById(categoryId);
+                    }
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
         return null;
     }
-
+    
     @Override
     public void update(int categoryId, Category category)
     {
         // update category
     }
-
+    
     @Override
     public void delete(int categoryId)
     {
         // delete category
     }
-
+    
     private Category mapRow(ResultSet row) throws SQLException
     {
         int    categoryId  = row.getInt("category_id");
         String name        = row.getString("name");
         String description = row.getString("description");
-
+        
         Category category = new Category()
         {{
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
         }};
-
+        
         return category;
     }
-
+    
 }
