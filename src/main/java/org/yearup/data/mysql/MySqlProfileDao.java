f3311e4e1333678fe1be823268cdb44a983fdb97
Marshall <mellisgibbs@gmail.com>
2024-06-27 10:46:19 -0400

    edited methods in 'MySqlProfileDao' to follow convention

--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -22,12 +22,12 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         
         try(
                 Connection connection = getConnection();
-                PreparedStatement statement = connection.prepareStatement(sql);
+                PreparedStatement ps = connection.prepareStatement(sql);
         )
         {
-            statement.setInt(1, userId);
+            ps.setInt(1, userId);
             
-            try(ResultSet row = statement.executeQuery();)
+            try(ResultSet row = ps.executeQuery();)
             {
                 if(row.next())
                 {
@@ -46,7 +46,7 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
     public Profile create(Profile profile)
     {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
         
         try(
                 Connection connection = getConnection();
@@ -89,20 +89,20 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         
         try(
                 Connection connection = getConnection();
-                PreparedStatement statement = connection.prepareStatement(sql);
+                PreparedStatement ps = connection.prepareStatement(sql);
         )
         {
-            statement.setString(1, profile.getFirstName());
-            statement.setString(2, profile.getLastName());
-            statement.setString(3, profile.getPhone());
-            statement.setString(4, profile.getEmail());
-            statement.setString(5, profile.getAddress());
-            statement.setString(6, profile.getCity());
-            statement.setString(7, profile.getState());
-            statement.setString(8, profile.getZip());
-            statement.setInt(9, profile.getUserId());
+            ps.setString(1, profile.getFirstName());
+            ps.setString(2, profile.getLastName());
+            ps.setString(3, profile.getPhone());
+            ps.setString(4, profile.getEmail());
+            ps.setString(5, profile.getAddress());
+            ps.setString(6, profile.getCity());
+            ps.setString(7, profile.getState());
+            ps.setString(8, profile.getZip());
+            ps.setInt(9, profile.getUserId());
             
-            statement.executeUpdate();
+            ps.executeUpdate();
         }
         catch (SQLException e)
         {
