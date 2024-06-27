aea57a10a2bc783efe7693e8f6612bd1e5e331a9
Marshall <mellisgibbs@gmail.com>
2024-06-27 17:20:51 -0400

    (WIP) started 'MySqlOrderDao' class

--- src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -1,5 +1,25 @@
 package org.yearup.data.mysql;
 
-public class MySqlOrderDao
+import org.springframework.stereotype.Component;
+import org.yearup.data.OrderDao;
+import org.yearup.models.Order;
+
+import javax.sql.DataSource;
+
+@Component
+public class MySqlOrderDao extends MySqlDaoBase implements OrderDao
 {
+    public MySqlOrderDao(DataSource dataSource)
+    {
+        super(dataSource);
+    }
+    
+    @Override
+    public Order create(int userId, Order order)
+    {
+        // Query string
+        // Inside try:
+            //
+        return null;
+    }
 }
